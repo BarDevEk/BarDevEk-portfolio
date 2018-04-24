f3578080c162142e338863d127a6506ba5cc1d66
Bartek Kisielewiczgit config --global user.name Bartek <bartkub5574@gmail.com>
2018-04-24 14:48:21 +0200

    scroll up button

--- global.js
+++ global.js
@@ -6,6 +6,16 @@
 		$('#linkAboutMe').click(function(){ $.scrollTo($('#aboutMe'),800); });
 		$('#linkContact').click(function(){ $.scrollTo($('#contact'),800); });
 		$('#linkProjects').click(function(){ $.scrollTo($('#projects'),800); });
+		$('#scrollUp').click(function(){ $.scrollTo($('#home'),800); });
 
 	}
 	);
+
+	$(window).scroll(
+
+			function(){
+				if($(this).scrollTop()>300) $('.scrollUp').fadeIn();
+				else $('.scrollUp').fadeOut();
+			}
+
+		);
